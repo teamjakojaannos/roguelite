87a1eb72a3b478162b4caa1a449b4e345cfc354c
Ilari Tommiska <ilari97@hotmail.com>
2019-11-11 05:57:57 +0200

    More Entity tests

--- src/test/java/fi/jakojäännös/roguelite/engine/ecs/EntityTest.java
+++ src/test/java/fi/jakojäännös/roguelite/engine/ecs/EntityTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EntityTest {
@@ -32,6 +34,13 @@ class EntityTest {
         assertTrue(entity.isMarkedForRemoval());
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -100, 101, 1000, 100})
+    void hasComponentBitThrowsWithInvalidBitIndex(int index) {
+        Entity entity = new Entity(0, 100);
+        assertThrows(IllegalArgumentException.class, () -> entity.hasComponentBit(index));
+    }
+
     @Test
     void hasComponentBitInitiallyReturnsFalseForAllValidBitsInMask() {
         Entity entity = new Entity(0, 100);
@@ -44,9 +53,125 @@ class EntityTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {-1, -100, 101, 1000, 100})
-    void hasComponentBitThrowsWithInvalidBitIndex(int index) {
+    @ValueSource(ints = {0, 1, 2, 69, 70})
+    void hasComponentBitReturnsTrueOnlyForOneComponentWhenOnlyOneIsAdded(int index) {
         Entity entity = new Entity(0, 100);
-        assertThrows(IllegalArgumentException.class, () -> entity.hasComponentBit(index));
+        entity.addComponentBit(index);
+        int countTrue = 0;
+        for (int i = 0; i < 100; ++i) {
+            countTrue += entity.hasComponentBit(i) ? 1 : 0;
+        }
+
+        assertEquals(1, countTrue);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 69, 70})
+    void hasComponentBitReturnsTrueForTheAddedComponentWhenOnlyOneIsAdded(int index) {
+        Entity entity = new Entity(0, 100);
+        entity.addComponentBit(index);
+        assertTrue(entity.hasComponentBit(index));
+    }
+
+    @Test
+    void hasComponentBitReturnsTrueForCorrectNumberOfComponentsWhenManyAreAdded() {
+        Entity entity = new Entity(0, 100);
+        entity.addComponentBit(0);
+        entity.addComponentBit(1);
+        entity.addComponentBit(2);
+        entity.addComponentBit(69);
+        entity.addComponentBit(70);
+        int countTrue = 0;
+        for (int i = 0; i < 100; ++i) {
+            countTrue += entity.hasComponentBit(i) ? 1 : 0;
+        }
+
+        assertEquals(5, countTrue);
+    }
+
+    @Test
+    void hasComponentBitReturnsTrueForAllAddedComponentsWhenManyAreAdded() {
+        Entity entity = new Entity(0, 100);
+        entity.addComponentBit(0);
+        entity.addComponentBit(1);
+        entity.addComponentBit(2);
+        entity.addComponentBit(69);
+        entity.addComponentBit(70);
+
+        assertTrue(entity.hasComponentBit(0));
+        assertTrue(entity.hasComponentBit(1));
+        assertTrue(entity.hasComponentBit(2));
+        assertTrue(entity.hasComponentBit(69));
+        assertTrue(entity.hasComponentBit(70));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -100, 101, 1000, 100})
+    void addComponentBitThrowsWithInvalidBitIndex(int index) {
+        Entity entity = new Entity(0, 100);
+        assertThrows(IllegalArgumentException.class, () -> entity.addComponentBit(index));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 69, 70})
+    void removeComponentBitDoesNothingIfRemovingComponentNotAddedWhenThereAreNoComponents(int index) {
+        Entity entity = new Entity(0, 100);
+        entity.removeComponentBit(index);
+
+        for (int i = 0; i < 100; ++i) {
+            assertFalse(entity.hasComponentBit(i));
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 69, 70})
+    void removeComponentBitDoesNothingIfRemovingComponentNotAddedWhenThereAreMultipleComponents(int index) {
+        List<Integer> added = List.of(3, 4, 6, 68, 71);
+        Entity entity = new Entity(0, 100);
+        added.forEach(entity::addComponentBit);
+        entity.removeComponentBit(index);
+
+        for (int i = 0; i < 100; ++i) {
+            if (added.contains(i)) {
+                assertTrue(entity.hasComponentBit(i));
+            } else {
+                assertFalse(entity.hasComponentBit(i));
+            }
+        }
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 69, 70})
+    void removeComponentBitRemovesTheComponentIfRemovingValidComponentWhenThereIsExactlyOneComponent(int index) {
+        Entity entity = new Entity(0, 100);
+        entity.addComponentBit(index);
+        entity.removeComponentBit(index);
+
+        for (int i = 0; i < 100; ++i) {
+            assertFalse(entity.hasComponentBit(i));
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 69, 70})
+    void removeComponentBitRemovesTheCorrectComponentIfRemovingValidComponentWhenThereAreMultipleComponents(int index) {
+        List<Integer> added = List.of(0, 1, 2, 69, 70);
+        Entity entity = new Entity(0, 100);
+        added.forEach(entity::addComponentBit);
+        entity.removeComponentBit(index);
+
+        for (int i = 0; i < 100; ++i) {
+            if (added.contains(i) && i != index) {
+                assertTrue(entity.hasComponentBit(i));
+            } else {
+                assertFalse(entity.hasComponentBit(i));
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -100, 101, 1000, 100})
+    void removeComponentBitThrowsWithInvalidBitIndex(int index) {
+        Entity entity = new Entity(0, 100);
+        assertThrows(IllegalArgumentException.class, () -> entity.removeComponentBit(index));
     }
 }
