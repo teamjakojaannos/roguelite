a2a1ace1070793454933f56af9879dee375dda86
Lassi Kuisma <lassi.kuisma1@gmail.com>
2019-11-26 20:50:09 +0200

    Unit test for HealthCheckSystem.

--- game/src/test/java/fi/jakojaannos/roguelite/game/systems/HealthCheckSystemTest.java
+++ game/src/test/java/fi/jakojaannos/roguelite/game/systems/HealthCheckSystemTest.java
@@ -1,4 +1,44 @@
 package fi.jakojaannos.roguelite.game.systems;
 
+import fi.jakojaannos.roguelite.engine.ecs.Entities;
+import fi.jakojaannos.roguelite.engine.ecs.Entity;
+import fi.jakojaannos.roguelite.engine.ecs.World;
+import fi.jakojaannos.roguelite.game.data.components.Health;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HealthCheckSystemTest {
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "1.0f,1.0f,0.0f,false",
+            "100.0f,-1.0f,0.0f,true",
+            "25.0f,25.0f,25.0f,true",
+            "25.0f,25.0f,24.0f,false",
+            "25.0f,25.0f,400.0f,true",
+            "100.0f,-5.0f,5.0f,true",
+            "100.0f,25.0f,25.0f,true",
+            "100.0f,25.0f,5.0f,false"
+    })
+    void entitiesWithZeroHpAreRemoved(double maxHp, double currentHp, double damage, boolean shouldBeRemoved) {
+        Entities entities = Entities.createNew(256, 32);
+        World world = World.createNew(entities);
+        HealthCheckSystem system = new HealthCheckSystem();
+
+        Entity entity = entities.createEntity();
+        Health hp = new Health(maxHp, currentHp);
+        entities.addComponentTo(entity, hp);
+        hp.currentHealth -= damage;
+
+        system.tick(Stream.of(entity), world, 0.2f);
+
+        assertEquals(shouldBeRemoved, entity.isMarkedForRemoval());
+    }
+
+
 }
--- game/src/test/java/fi/jakojaannos/roguelite/game/test/ArchetypeTest.java
+++
@@ -1,4 +0,0 @@
-package fi.jakojaannos.roguelite.game.test;
-
-public class ArchetypeTest {
-}
