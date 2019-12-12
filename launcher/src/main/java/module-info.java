4a4d36a047ca68aa4d85292beb44da776659da2d
Ilari Tommiska <ilari97@hotmail.com>
2019-12-12 10:44:02 +0200

    Updated buildscripts to allow packaging standalone images with jlink

--- launcher/src/main/java/module-info.java
+++ launcher/src/main/java/module-info.java
@@ -3,4 +3,9 @@ module roguelite.launcher {
     requires org.slf4j;
     requires roguelite.engine;
     requires roguelite.game;
+
+    // Make sure jlink packages the natives
+    requires org.lwjgl.natives;
+    requires org.lwjgl.glfw.natives;
+    requires org.lwjgl.opengl.natives;
 }
