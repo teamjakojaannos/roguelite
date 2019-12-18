02748e7ac12c37547b31b5c03028f12d5bbdf608
Lassi Kuisma <lassi.kuisma1@gmail.com>
2019-12-18 14:18:10 +0200

    Tests for slimes

---
+++ game/src/test/java/fi/jakojaannos/roguelite/game/systems/SlimeAIControllerSystemTest.java
@@ -0,0 +1,67 @@
+package fi.jakojaannos.roguelite.game.systems;
+
+import fi.jakojaannos.roguelite.engine.ecs.Entity;
+import fi.jakojaannos.roguelite.engine.ecs.EntityManager;
+import fi.jakojaannos.roguelite.engine.ecs.World;
+import fi.jakojaannos.roguelite.game.data.archetypes.PlayerArchetype;
+import fi.jakojaannos.roguelite.game.data.archetypes.SlimeArchetype;
+import fi.jakojaannos.roguelite.game.data.components.SlimeAI;
+import fi.jakojaannos.roguelite.game.data.components.SlimeSharedAI;
+import fi.jakojaannos.roguelite.game.data.components.Transform;
+import fi.jakojaannos.roguelite.game.data.resources.Players;
+import org.joml.Vector2d;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SlimeAIControllerSystemTest {
+
+    @Test
+    void smallerSlimesCanRegroupIntoLargerSlime() {
+        SlimeAIControllerSystem system = new SlimeAIControllerSystem();
+        EntityManager entityManager = EntityManager.createNew(256, 32);
+        World world = World.createNew(entityManager);
+
+        world.getResource(Players.class).player =
+                PlayerArchetype.create(entityManager, new Transform(100, 100));
+
+
+        Vector2d vec = new Vector2d(0.0, 0.0);
+        SlimeSharedAI sharedAI = new SlimeSharedAI();
+        List<Entity> list = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            Entity slime = SlimeArchetype.createSmallSlimeWithInitialVelocity(entityManager, 0.0, 0.0, vec, 0.0);
+            entityManager.addComponentTo(slime, sharedAI);
+            sharedAI.slimes.add(slime);
+            list.add(slime);
+        }
+
+        sharedAI.regroupPos.set(0.0, 0.0);
+        sharedAI.regrouping = true;
+
+        system.tick(list.stream(), world, 0.2);
+        entityManager.applyModifications();
+
+        List<EntityManager.EntityComponentPair<SlimeAI>> entities =
+                entityManager
+                        .getEntitiesWith(SlimeAI.class)
+                        .collect(Collectors.toList());
+
+
+        assertEquals(1, entities.size());
+
+        boolean hasOneLargeSlime =
+                (entities.size() == 1
+                        && entities.get(0).getComponent().slimeSize == 2);
+
+        assertTrue(hasOneLargeSlime);
+
+    }
+
+}
---
+++ game/src/test/java/fi/jakojaannos/roguelite/game/systems/SlimeDeathHandlerSystemTest.java
@@ -0,0 +1,69 @@
+package fi.jakojaannos.roguelite.game.systems;
+
+import fi.jakojaannos.roguelite.engine.ecs.Entity;
+import fi.jakojaannos.roguelite.engine.ecs.EntityManager;
+import fi.jakojaannos.roguelite.engine.ecs.World;
+import fi.jakojaannos.roguelite.game.data.archetypes.SlimeArchetype;
+import fi.jakojaannos.roguelite.game.data.components.DeadTag;
+import fi.jakojaannos.roguelite.game.data.components.SlimeAI;
+import fi.jakojaannos.roguelite.game.data.components.SlimeSharedAI;
+import org.joml.Vector2d;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SlimeDeathHandlerSystemTest {
+
+    @Test
+    void largeSlimeSpawnsMultipleSlimesOnDeath() {
+        SlimeDeathHandlerSystem system = new SlimeDeathHandlerSystem();
+        EntityManager entityManager = EntityManager.createNew(256, 32);
+        World world = World.createNew(entityManager);
+
+        Entity slime = SlimeArchetype.createLargeSlime(entityManager, 0.0, 0.0);
+        entityManager.addComponentTo(slime, new DeadTag());
+
+        entityManager.applyModifications();
+
+        long amountBefore = entityManager.getEntitiesWith(SlimeAI.class).count();
+        system.tick(Stream.of(slime), world, 0.2);
+        entityManager.applyModifications();
+        long amountAfter = entityManager.getEntitiesWith(SlimeAI.class).count();
+
+        assertTrue(amountAfter > amountBefore);
+    }
+
+
+    @Test
+    void sharedAIComponentIsRemovedOnDeath() {
+        SlimeDeathHandlerSystem system = new SlimeDeathHandlerSystem();
+        EntityManager entityManager = EntityManager.createNew(256, 32);
+        World world = World.createNew(entityManager);
+
+        Vector2d vec = new Vector2d(0.0, 0.0);
+        Entity slime1 = SlimeArchetype.createSmallSlimeWithInitialVelocity(entityManager, 0.0, 0.0, vec, 0.0);
+        Entity slime2 = SlimeArchetype.createSmallSlimeWithInitialVelocity(entityManager, 0.0, 0.0, vec, 0.0);
+        Entity slime3 = SlimeArchetype.createSmallSlimeWithInitialVelocity(entityManager, 0.0, 0.0, vec, 0.0);
+        Entity slime4 = SlimeArchetype.createSmallSlimeWithInitialVelocity(entityManager, 0.0, 0.0, vec, 0.0);
+
+        SlimeSharedAI sharedAI = new SlimeSharedAI();
+        entityManager.addComponentTo(slime1, sharedAI);
+        entityManager.addComponentTo(slime2, sharedAI);
+        entityManager.addComponentTo(slime3, sharedAI);
+        entityManager.addComponentTo(slime4, sharedAI);
+
+        sharedAI.slimes.add(slime1);
+        sharedAI.slimes.add(slime2);
+        sharedAI.slimes.add(slime3);
+        sharedAI.slimes.add(slime4);
+
+        entityManager.addComponentTo(slime4, new DeadTag());
+        system.tick(Stream.of(slime4), world, 0.2);
+
+        assertTrue(sharedAI.slimes.size() == 3
+                && !sharedAI.slimes.contains(slime4));
+    }
+
+}
